a58dd00b202a08ccc063548b0b148c7e69a2e11b
cjwade <89665017+cjwade@users.noreply.github.com>
2022-09-21 19:52:10 -0700

    Add landing, featured, and discount sections

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import Discounted from "./components/Discounted";
+import Featured from "./components/Featured";
+import Highlights from "./components/Highlights";
+import Landing from "./components/Landing";
 import Nav from "./components/Nav";
 
 function App() {
 	return (
 		<div className="App">
 			<Nav />
+			<Landing />
+			<Highlights />
+			<Featured />
+			<Discounted />
 		</div>
 	);
 }
